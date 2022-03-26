7e9c14157b403c6f3a8bc1926ae5df1e789c4b54
Pat Lawler <patlaw777@icloud.com>
2022-03-26 12:49:53 -0500

    fixed heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'techie blogs are cool',
+  secret: process.env.SESSION_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
